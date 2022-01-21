0d6e7f23a0ef5ecefc42d7d6410ab503d82c15c7
Laiba Sajid Khan <sajid.laiba378@gmail.com>
2022-01-21 18:08:06 -0500

    removed React.strictmode

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
       <NavBar/>
       <main>
         <Routes>
-            <Route exact path="/" element={<Home/>}/>
+            <Route path="/" element={<Home/>}/>
             <Route path="/transactions" element={<Index/>}/>
             <Route path="/transactions/new" element={<New/>}/>
             <Route path="/transactions/:index" element={<Show/>}/>
